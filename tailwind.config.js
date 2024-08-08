ea8cbd30b0c0a933bd833bc947a4b1593352b8f9
Edward Brock <1416477069@qq.com>
2024-08-08 23:26:48 +0800

    配置 tailwind 文件； 更改衬线字体；

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', 'Inter', 'Avenir', 'Bahnschrift', 'Tahoma', 'Helvetica Neue', 'Segoe UI', 'Arial', 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'WenQuanYi Micro Hei', 'sans-serif', 'Apple Logo'],
+      },
+    },
+  },
+  plugins: [],
+}
