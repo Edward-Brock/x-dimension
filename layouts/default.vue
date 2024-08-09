5cf91791842f18b6a69348c6a4b5ccde39ba1d70
Edward Brock <1416477069@qq.com>
2024-08-09 21:40:33 +0800

    添加 default.vue 默认布局； 新增默认 AppHeader.vue 文件；

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppHeader from '~/components/header/AppHeader.vue'
+</script>
+
+<template>
+  <div>
+    <header class="bg-background/75 backdrop-blur border-b -mb-px sticky top-0 z-50 border-gray-200 dark:border-gray-800">
+      <AppHeader />
+    </header>
+
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
